9c2afc2d15c0b4557171fb3c0c8754e653843f49
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2025-04-07 22:59:45 +0900

    Initial Commit

---
+++ MyVideoPictureInPicture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyVideoPictureInPicture
+//
+//  Created by Kenta Matsue on 2025/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyVideoPictureInPictureTests/MyVideoPictureInPictureTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyVideoPictureInPictureTests.swift
+//  MyVideoPictureInPictureTests
+//
+//  Created by Kenta Matsue on 2025/04/07.
+//
+
+import Testing
+@testable import MyVideoPictureInPicture
+
+struct MyVideoPictureInPictureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
